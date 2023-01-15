d1b22dfdb6680a9389d487db1c31fcd392038f8b
Jenny-Lim <180jnlm@gmail.com>
2023-01-15 10:57:57 -0500

    update piece + pawn

--- ToroksGambit/Assets/Scripts/Pawn.cs
+++ ToroksGambit/Assets/Scripts/Pawn.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Pawn : Piece
 {
+    private bool isFirstMove;
+    private bool capturing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        type = "pawn";
+        isFirstMove = true;
+        capturing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //validMoves[pieceX, pieceY + 1] = true;
+        moves.Add(new Move(pieceX, pieceY + 1));
+
+        if (capturing)
+        {
+            //validMoves[pieceX + 1, pieceY + 1] = true;
+            moves.Add(new Move(pieceX + 1, pieceY + 1));
+        }
+        if (isFirstMove)
+        {
+            //validMoves[pieceX, pieceY + 2] = true;
+            moves.Add(new Move(pieceX, pieceY + 2));
+
+            // somewhere we have to say isFirstMove = false;
+        }
     }
 }
--- ToroksGambit/Assets/Scripts/Piece.cs
+++ ToroksGambit/Assets/Scripts/Piece.cs
@@ -5,9 +5,43 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
 
-    private bool isTaken;
-    private int pieceX;
-    private int pieceY;
-    private string type;
+    public class Move
+    {
+        public int newX;
+        public int newY;
 
+        public Move(int newX, int newY)
+        {
+            this.newX = newX;
+            this.newY = newY;
+        }
+    }
+
+    public List<Move> moves;
+
+    public bool isTaken;
+    public int pieceX;
+    public int pieceY;
+    public string type;
+
+    void Awake()
+    {
+        moves = new List<Move>();
+        pieceX = getX();
+        pieceY = getY();
+    }
+
+    int getX()
+    {
+        // get current location of piece -- ask patrick
+        return 0;
+        // 0 for now
+    }
+
+    int getY()
+    {
+        // get current location of piece
+        return 0;
+        // 0 for now
+    }
 }
