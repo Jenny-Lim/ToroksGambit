d90ac5fb5cf1de5dc565748fab7fe93373d6c091
PatrickLBrock <patrickleonard0312@gmail.com>
2023-01-17 10:22:23 -0500

    move updated stuff
    can click off board to clear stored piece

--- ToroksGambit/Assets/Scripts/Board.cs
+++ ToroksGambit/Assets/Scripts/Board.cs
@@ -88,7 +88,7 @@ public class Board : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            if(hit.transform.tag == "Chess Piece")//if mouse is clicked on chess piece
+                if (hit.transform.tag == "Chess Piece")//if mouse is clicked on chess piece
             {
                 storedPiece = hit.transform.gameObject;//store piece
 
@@ -125,7 +125,14 @@ public class Board : MonoBehaviour
                 MovePiece(pieceX, pieceY, clickedX, clickedY);
                 storedPiece.transform.position = hit.transform.position + new Vector3(0,0,0);
 
-            }
+                storedPiece = null;
+
+               }
+        }
+        else 
+        {
+                storedPiece = null;
+                Debug.Log("didnt hit anything");    
         }
         }
         if (Input.GetMouseButtonDown(1))//right click mouse to undo moves
