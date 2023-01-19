b694bfc496215f6fb08f238b59c2060b5529a486
Jenny-Lim <180jnlm@gmail.com>
2023-01-19 17:00:52 -0500

    knight has other half of moves now lmao

--- ToroksGambit/Assets/Scripts/Pieces/Knight.cs
+++ ToroksGambit/Assets/Scripts/Pieces/Knight.cs
@@ -30,6 +30,23 @@ public class Knight : Piece
         {
             moves.Add(new Move(pieceX, pieceY, pieceX - 2, pieceY - 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 2, pieceY - 1]));
         }
+
+        if (InBoundsCheck(pieceX + 1, pieceY - 2))
+        {
+            moves.Add(new Move(pieceX, pieceY, pieceX + 1, pieceY - 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 1, pieceY - 2]));
+        }
+        if (InBoundsCheck(pieceX + 2, pieceY - 1))
+        {
+            moves.Add(new Move(pieceX, pieceY, pieceX + 2, pieceY - 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 2, pieceY - 1]));
+        }
+        if (InBoundsCheck(pieceX - 1, pieceY + 2))
+        {
+            moves.Add(new Move(pieceX, pieceY, pieceX - 1, pieceY + 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 1, pieceY + 2]));
+        }
+        if (InBoundsCheck(pieceX - 2, pieceY + 1))
+        {
+            moves.Add(new Move(pieceX, pieceY, pieceX - 2, pieceY + 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 2, pieceY + 1]));
+        }
     }
 
 }
