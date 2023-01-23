3f2eec25dcbd42ff0e328c7c5c171713b63f9bc7
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-01-22 22:04:30 -0500

    gameState clean up

--- ToroksGambit/Assets/Scripts/GameStateManager.cs
+++ ToroksGambit/Assets/Scripts/GameStateManager.cs
@@ -13,12 +13,6 @@ public class GameStateManager : MonoBehaviour
     }
     [SerializeField] private GameState currentState;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
