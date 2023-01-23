ee365f382802d4249efc98b9f82478334321e2b6
Jenny-Lim <180jnlm@gmail.com>
2023-01-22 23:48:44 -0500

    knight / piece fix
    knight no longer able to move to spot occupied by same color piece, wall or hole

--- ToroksGambit/Assets/Scripts/Pieces/Knight.cs
+++ ToroksGambit/Assets/Scripts/Pieces/Knight.cs
@@ -14,36 +14,36 @@ public class Knight : Piece
     {
         moves.Clear();
 
-        if (InBoundsCheck(pieceX + 1, pieceY + 2))
+        if (InBoundsCheck(pieceX + 1, pieceY + 2) && ClearCheck(pieceX, pieceY, pieceX + 1, pieceY  + 2))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX + 1, pieceY + 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 1, pieceY + 2]));
         }
-        if (InBoundsCheck(pieceX + 2, pieceY + 1))
+        if (InBoundsCheck(pieceX + 2, pieceY + 1) && ClearCheck(pieceX, pieceY, pieceX + 2, pieceY + 1))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX + 2, pieceY + 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 2, pieceY + 1]));
         }
-        if (InBoundsCheck(pieceX - 1, pieceY - 2))
+        if (InBoundsCheck(pieceX - 1, pieceY - 2) && ClearCheck(pieceX, pieceY, pieceX - 1, pieceY - 2))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX - 1, pieceY - 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 1, pieceY - 2]));
         }
-        if (InBoundsCheck(pieceX - 2, pieceY - 1))
+        if (InBoundsCheck(pieceX - 2, pieceY - 1) && ClearCheck(pieceX, pieceY, pieceX - 2, pieceY - 1))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX - 2, pieceY - 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 2, pieceY - 1]));
         }
 
-        if (InBoundsCheck(pieceX + 1, pieceY - 2))
+        if (InBoundsCheck(pieceX + 1, pieceY - 2) && ClearCheck(pieceX, pieceY, pieceX + 1, pieceY - 2))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX + 1, pieceY - 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 1, pieceY - 2]));
         }
-        if (InBoundsCheck(pieceX + 2, pieceY - 1))
+        if (InBoundsCheck(pieceX + 2, pieceY - 1) && ClearCheck(pieceX, pieceY, pieceX + 2, pieceY - 1))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX + 2, pieceY - 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX + 2, pieceY - 1]));
         }
-        if (InBoundsCheck(pieceX - 1, pieceY + 2))
+        if (InBoundsCheck(pieceX - 1, pieceY + 2) && ClearCheck(pieceX, pieceY, pieceX - 1, pieceY + 2))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX - 1, pieceY + 2, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 1, pieceY + 2]));
         }
-        if (InBoundsCheck(pieceX - 2, pieceY + 1))
+        if (InBoundsCheck(pieceX - 2, pieceY + 1) && ClearCheck(pieceX, pieceY, pieceX - 2, pieceY + 1))
         {
             moves.Add(new Move(pieceX, pieceY, pieceX - 2, pieceY + 1, pieceBoard[pieceX, pieceY], pieceBoard[pieceX - 2, pieceY + 1]));
         }
