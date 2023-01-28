11a91be9260cf699250aef47ac944f594fcf0f99
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-01-28 16:17:03 -0500

    fix compilation error

--- ToroksGambit/Assets/Scripts/Interrupts/AddPieceInterrupt.cs
+++ ToroksGambit/Assets/Scripts/Interrupts/AddPieceInterrupt.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AddPieceInterrupt", menuName = "ScriptableObjects/AddPieceInterrupt", order = 1)]
 public class AddPieceInterrupt : ScriptableObject, IInterrupt
 {
-    publicint PieceType;
+    public int PieceType;
     public Vector2 placeAt;
     public int placeOnTurn;
 
