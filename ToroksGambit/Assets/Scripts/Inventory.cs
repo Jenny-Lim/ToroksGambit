439bde3da5644d9049093cbda18990c2579448c5
PatrickLBrock <patrickleonard0312@gmail.com>
2023-03-06 21:29:25 -0500

    screw it, you get 20 pieces

--- ToroksGambit/Assets/Scripts/Inventory.cs
+++ ToroksGambit/Assets/Scripts/Inventory.cs
@@ -19,8 +19,8 @@ public class Inventory : MonoBehaviour
     private RectTransform rectTrans;
     [SerializeField] private TextMeshProUGUI hideShowText;
     [SerializeField] private GameObject hideShowButton;
-    [SerializeField] private int[] maxHeldPieces = {5,5,5,5,1};//the maximum number of each piece the player can have
-    private int[] heldPieces = {4,4,4,4,1};//the amount of each piece the player has
+    [SerializeField] private int[] maxHeldPieces = {50,50,50,50,10};//the maximum number of each piece the player can have
+    private int[] heldPieces = {20,20,20,20,10};//the amount of each piece the player has
     [SerializeField] private float ghostPieceVertOffset = -0.05f;
     private bool infinitePieces = true;
     [SerializeField] public GameObject objectiveArea;
