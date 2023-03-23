a101b4a91d2b61eec088f23197edcf2f5d877eb1
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-03-23 07:27:22 -0400

    ticket particle thingy not finished

---
+++ ToroksGambit/Assets/Scripts/UIParticleSystem/UIParticle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIParticle : MonoBehaviour
+{
+    private Vector2 velocity;
+    private bool moveToTicketCounter = false;
+
+    public void UpdateParticle(float deltaTime, Transform moveTowards)
+    {
+        UpdateScale(deltaTime);
+
+        //if (moveToTicketCounter)
+        //{
+        MovePosition(deltaTime, moveTowards);
+        //UpdateVelocity(deltaTime, moveTowards);
+        //}
+    }
+
+    private void UpdateScale(float deltaTime)
+    {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.one, deltaTime);
+        moveToTicketCounter = transform.localScale == Vector3.one;
+    }
+
+    private void MovePosition(float deltaTime, Transform moveTowards)
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, moveTowards.localPosition, velocity.magnitude * deltaTime);
+        //transform.localPosition += (Vector3)velocity * deltaTime;
+    }
+
+    public Vector2 GetVelocity()
+    {
+        return velocity;
+    }
+
+    public void SetVelocity(Vector2 newVel)
+    {
+        velocity = newVel;
+    }
+
+    public void UpdateVelocity(float deltaTime, Transform moveTowards)
+    {
+        velocity = Vector3.RotateTowards(velocity, (Vector2)moveTowards.localPosition, 3.14f, 1f);
+    }
+}
---
+++ ToroksGambit/Assets/Scripts/UIParticleSystem/UIParticleSystem.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class UIParticleSystem : MonoBehaviour
+{
+    struct SpawnBox {
+        public Vector3 center;
+        public Vector3 bounds;
+
+        public SpawnBox(Vector3 newCenter, Vector3 newBounds)
+        {
+            center = newCenter;
+            bounds = newBounds;
+        }
+    };
+
+    [Tooltip("The object that the tickets will move towards")]
+    [SerializeField] Transform target;
+    [Space(4)]
+
+    [SerializeField] private GameObject particlePrefab;//holds the prefab that represents a particle
+    private List<UIParticle> particles;//the list of all active particles
+    [Space(4)]
+
+    public bool isEmitting = false;//whether to emit more particles, if false does not delete/remove particles see StopAndClear()
+    [Space(4)]
+
+    [SerializeField] private int maxParticles = -1;//the maximum number of particles that can be active at once, -1 means infinite
+    [SerializeField] private float spawnDelay = 1;//how much time to wait before trying to spawn another particle
+    [Space(4)]
+
+    private SpawnBox spawnArea;
+    [SerializeField] private Vector3 spawnAreaCenter;
+    [SerializeField] private Vector3 spawnAreaBounds;
+    [Space(4)]
+
+    [SerializeField] private int minSpawnAmount = 1;//the minimum number of particles that can spawn when spawning (does NOT go over maxParticles)
+    [SerializeField] private int maxSpawnAmount = 3;//the maximum number of particles that can spawn when spawning (does NOT go over maxParticles)
+    [Space(4)]
+
+    [SerializeField] private float maxParticleSpeed = 20;
+    [SerializeField] private float minParticleSpeed = 1;
+
+    private int particleCount { get { return particles.Count; } }//returns the number of active particles 
+
+
+    private void Awake()
+    {
+
+        if (maxParticles > -1)
+        {
+            particles = new List<UIParticle>(maxParticles);
+        }
+        else
+        {
+            particles = new List<UIParticle>(20);//default amount
+        }
+        spawnArea = new SpawnBox(spawnAreaCenter, spawnAreaBounds);
+
+        while (particleCount < maxParticles)
+        {
+            SpawnParticle();
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Log(target.position);
+    }
+
+    private void Update()
+    {
+        if (isEmitting)
+        {
+            //emit particle
+        }
+
+        if (particleCount > 0)
+        {
+            UpdateParticles(Time.deltaTime, target);
+        }
+    }
+
+    public void Emit()
+    {
+        isEmitting = true;
+    }
+    public void Stop()
+    {
+        isEmitting = false;
+    }
+
+    public void StopAndClear()
+    {
+        isEmitting = false;
+        particles.Clear();
+    }
+
+    private void DeleteOldestParticle()
+    {
+
+    }
+
+    private void DeleteParticle(int index)
+    {
+
+    }
+
+    private void DeleteParticle()
+    {
+
+    }
+
+    private void SpawnParticle()
+    {
+        if (particleCount >= maxParticles) { return; }
+        UIParticle particle = Instantiate(particlePrefab, Vector3.zero, Quaternion.identity, transform).GetComponent<UIParticle>();
+        InitParticle(ref particle);
+        particles.Add(particle);
+    }
+
+    private Vector3 FindSpawnLocation()
+    {
+        Vector3 pos = new Vector3();
+
+        pos.x = (int)Random.Range(spawnArea.center.x - spawnAreaBounds.x, spawnArea.center.x + spawnAreaBounds.x);
+        pos.y = (int)Random.Range(spawnArea.center.y - spawnAreaBounds.y, spawnArea.center.y + spawnAreaBounds.y);
+        pos.z = (int)Random.Range(spawnArea.center.z - spawnAreaBounds.z, spawnArea.center.z + spawnAreaBounds.z);
+
+        return pos;
+    }
+
+    private Vector2 RandomParticleVelocity()
+    {
+        Vector2 vel = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+        vel *= maxParticleSpeed;
+        vel.x = Mathf.Max(vel.x, minParticleSpeed);
+        vel.y = Mathf.Max(vel.y, minParticleSpeed);
+        return vel;
+    }
+
+    private void UpdateParticles(float deltaTime, Transform moveTowards)
+    {
+        foreach (UIParticle particle in particles)
+        {
+            particle.UpdateParticle(deltaTime, moveTowards);
+        }
+    }
+
+    private void InitParticle(ref UIParticle particle)
+    {
+        particle.transform.localPosition = FindSpawnLocation();
+        particle.SetVelocity(RandomParticleVelocity());
+        particle.transform.localScale = Vector3.zero;
+    }
+}
