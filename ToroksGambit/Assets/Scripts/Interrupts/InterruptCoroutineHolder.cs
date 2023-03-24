69eeddea6c1616ce4589ca0286f3104fd63fa2d9
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-03-24 11:17:58 -0400

    stuff

--- ToroksGambit/Assets/Scripts/Interrupts/InterruptCoroutineHolder.cs
+++ ToroksGambit/Assets/Scripts/Interrupts/InterruptCoroutineHolder.cs
@@ -24,7 +24,6 @@ public class InterruptCoroutineHolder : MonoBehaviour
             return;
         }
 
-        StopAllCoroutines();
         switch (coroutineType)
         {
             case Coroutines.AddPiece:
