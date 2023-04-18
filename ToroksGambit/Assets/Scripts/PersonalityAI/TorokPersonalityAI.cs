e0f8bad0681585c07168c728569d40515d961fac
Jordan-p20 <71034120+Jordan-p20@users.noreply.github.com>
2023-04-17 22:05:09 -0400

    yEEEEEEEEEEEEEEEEEEETTTTTTTTTT

--- ToroksGambit/Assets/Scripts/PersonalityAI/TorokPersonalityAI.cs
+++ ToroksGambit/Assets/Scripts/PersonalityAI/TorokPersonalityAI.cs
@@ -14,7 +14,7 @@ public class TorokPersonalityAI : MonoBehaviour
 
     [SerializeField] private int currentAngerLevel = 1;
     [Range(0f, 1f)]
-    [SerializeField] private float[] dialogLikelyhoodByCategory = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; // 17 total (19 now -jenny)
+    [SerializeField] private float[] dialogLikelyhoodByCategory = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; // 19 total
     private int[] categoryPriorities = { 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 3, 3, 3, 3, 2, 2, 2 };
     public float minTimeBetweenIdleBark = 12;
     public float maxTimeBetweenIdleBark = 25;
